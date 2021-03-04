ccb292b2a2d17b5bf8524b6f98522ca0c5d887fe
amandiu <amanullah15-2447@diu.edu.bd>
2021-03-04 19:40:50 +0600

    Create Contact page
    none

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Header from './component/Header'
 import Footer from './component/Footer'
 import Cart from './component/Cart'
-import Profile from './component/Profile'
+import Contact from'./component/Contact'
 function App() { 
   var v="Convert any RGB value to its Hex color code, along with corresponding HSL, HSV and CMYK values (including HTML/CSS values). Convert any RGB value to its Hex color code, along with corresponding HSL, ";
   //card has head,user,viewer,img,time
@@ -11,7 +11,8 @@ function App() {
     <div className="App">
       <Header></Header>
       <div className="body">
-        <Profile></Profile>
+        <Contact head></Contact> 
+                
       </div>
       <Footer></Footer>
     </div>
---
+++ src/component/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Contact.css';
+function Contact(){
+    return(
+        <div className='cnt'>
+        <div className='cnt-2'> 
+        <h1><u>Contact</u></h1>
+        </div>
+        </div>
+    );
+}
+export default Contact;
